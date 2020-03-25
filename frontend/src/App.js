0a1c0c5e91e609722891ae9f107b3a4d0a94d7dc
gilmar.silva <gilmar.silva@mpgo.mp.br>
2020-03-25 18:50:48 -0300

    propriedade e statdo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incremente() {
+    setCounter(counter + 1);
+    
+  }
+
   return (
-   <h1>Hello world</h1>
+    <div>
+      <Header> semana de terste: {counter}</Header>
+      <button onClick={incremente}>Incrementar</button>
+    </div>
   );
 }
 
