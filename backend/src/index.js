8b8c05c67327381d5465a3e14739c97693b121fb
gilmar.silva <gilmar.silva@mpgo.mp.br>
2020-03-27 11:16:17 -0300

    ajuste pois nao etava rodando

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const cors = express("cors")
-const routes = require("./routes")
+const cors = require("cors");
 
+const routes = require("./routes");
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(routes);
-
 app.listen(3333);
